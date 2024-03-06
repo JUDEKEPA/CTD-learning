84396e5ceb8d65444386f61198d8e38734a61f23
Zhengdi Liu <294990806@qq.com>
2024-03-06 23:55:56 +0800

    update

--- notes/code learning/gibbsenergy_calculation.py
+++ notes/code learning/gibbsenergy_calculation.py
@@ -1,12 +1,13 @@
 from pycalphad.core.calculate import calculate
 from pycalphad.io.database import Database
 from pycalphad.core.utils import instantiate_models
+from pycalphad.codegen.callables import build_phase_records
 
 db_alzn = Database('cfe_broshe.tdb') #''..//cfe_broshe.tdb alzn_mey.tdb
 
 #my_phases_alzn = ['LIQUID', 'FCC_A1', 'HCP_A3']
 models = instantiate_models(db_alzn, ['Fe', 'C', 'VA'], ['BCC_A2'], model=None, parameters=dict())
-# phase_records = build_phase_records(dbf, comps, active_phases, statevar_dict,
+# phase_records = build_phase_records(db_alzn, ['Fe', 'C', 'VA'], ['BCC_A2'], statevar_dict,
 #                                             models=models, parameters=parameters,
 #                                             output=output, callables=callables,
 #                                             build_gradients=False, build_hessians=False,
